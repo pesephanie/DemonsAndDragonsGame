dbc6e8f0c3709af1600f79d7d92636a1fa620444
YReyneke <nic.sam.yac@gmail.com>
2022-02-28 16:02:01 +0000

    Collab Version
    Updated to latest files and setup files for collaboration

---
+++ ProjectFiles/Source/DemonsAndDragons/DD_PressurePlate.h
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DD_PressurePlate.generated.h"
+
+UCLASS()
+class DEMONSANDDRAGONS_API ADD_PressurePlate : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADD_PressurePlate();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Custom Blueprint Events
+	
+	/*
+	* Called once there is sufficient mass 'on' the plate
+	*/
+	UFUNCTION(BlueprintImplementableEvent, Category = "Pressure Plate")
+	void TriggerEffectStart();
+
+	/*
+	* Called when there is no sufficient mass 'on' the plate
+	*/
+	UFUNCTION(BlueprintImplementableEvent, Category = "Pressure Plate")
+	void TriggerEffectEnd();
+
+	// Components
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pressure Plate")
+	class UStaticMeshComponent* PressurePlateMesh;
+
+	/*
+	* The collision volume that must contain enough weighted objects for the plate to trigger
+	*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pressure Plate")
+	class UBoxComponent* PressureVolume;
+
+	/*
+	* The total weight required on this plate to trigger the active state
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pressure Plate")
+	float TriggerWeight{ 20.f };
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pressure Plate")
+	float CurrentWeightOnPlate{ 0.f };
+
+private:
+	/*
+	* Current trigger state
+	*/
+	UPROPERTY()
+	bool bTriggered{ false };
+
+	/*
+	* List of all crates putting pressure on this plate
+	*/
+	UPROPERTY()
+	TArray<class ADD_PuzzleCrate*> PressureCrateList{};
+
+	/*
+	* Flush and refresh plate list as required
+	* Also updates CurrentWeightOnPlate
+	*/
+	UFUNCTION()
+	void UpdatePressureWeight();
+
+public:
+	/*
+	* Function that is called from crate when adding to list
+	*/
+	UFUNCTION()
+	void AddCrate(class ADD_PuzzleCrate* CrateRef);
+
+	/*
+	* Function that is called from crate when adding to list
+	*/
+	UFUNCTION()
+	void RemoveCrate(class ADD_PuzzleCrate* CrateRef);
+};
---
+++ ProjectFiles/Source/DemonsAndDragons/DD_PuzzleCrate.cpp
@@ -0,0 +1,123 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DD_PuzzleCrate.h"
+#include "DD_PlayerCharacter.h"
+#include "DD_PressurePlate.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ADD_PuzzleCrate::ADD_PuzzleCrate()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CrateMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Moveable Crate Mesh"));
+	SetRootComponent(CrateMesh);
+
+	WeightingCollisionVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("Weighting Collision Volume"));
+	WeightingCollisionVolume->SetupAttachment(CrateMesh);
+
+	// Setup delegate functions for binding
+	BeginCrateOL.BindUFunction(this, "BeginOverlapWithOther");
+	EndCrateOL.BindUFunction(this, "EndOverlapWithOther");
+}
+
+// Called when the game starts or when spawned
+void ADD_PuzzleCrate::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Bind Delegates
+	WeightingCollisionVolume->OnComponentBeginOverlap.Add(BeginCrateOL);
+	WeightingCollisionVolume->OnComponentEndOverlap.Add(EndCrateOL);
+}
+
+// Called every frame
+void ADD_PuzzleCrate::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ADD_PuzzleCrate::InteractWith(ADD_PlayerCharacter* PlayerCharacter) {
+	PlayerCharacterRef = PlayerCharacter;
+	CrateMesh->SetSimulatePhysics(false); // For the sake of simplicity
+	PlayerCharacterRef->AttachCarryObject(this);
+	PickUp();
+}
+
+void ADD_PuzzleCrate::StopInteraction() {
+	if (PlayerCharacterRef) {
+		PlayerCharacterRef->DetachCarryObject();
+		PlayerCharacterRef = nullptr; // Unlink from carrying player
+		CrateMesh->SetSimulatePhysics(true); // Just re-enable to be safe
+	}
+	PutDown();
+}
+
+void ADD_PuzzleCrate::BeginOverlapWithOther(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult) {
+
+	ADD_PressurePlate* Plate{ Cast<ADD_PressurePlate>(OtherActor) };
+	if (Plate != nullptr) {
+		//UE_LOG(LogTemp, Warning, TEXT("%s: Begin overlap with plate: %s"), *GetName(), *Plate->GetName());
+		PlateRef = Plate;
+		PlateRef->AddCrate(this);
+		return;
+	}
+	//UE_LOG(LogTemp, Warning, TEXT("Crate Begin Overlap: Plate Invalid"));
+
+	ADD_PuzzleCrate* OtherCrate{ Cast<ADD_PuzzleCrate>(OtherActor) };
+	if (OtherCrate != nullptr) {
+		//UE_LOG(LogTemp, Warning, TEXT("%s: Begin overlap with crate: %s"), *GetName(), *OtherCrate->GetName());
+		if (PlateRef == nullptr && OtherCrate->PlateRef != nullptr) {
+			PlateRef = OtherCrate->PlateRef;
+			PlateRef->AddCrate(this);
+			return;
+		}
+	}
+	//UE_LOG(LogTemp, Warning, TEXT("Crate Begin Overlap: Other Crate Invalid/Plate Ref Already Valid/Other Crate Plate Ref Invalid"));
+}
+
+void ADD_PuzzleCrate::EndOverlapWithOther(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
+	
+	ADD_PressurePlate* Plate{ Cast<ADD_PressurePlate>(OtherActor) };
+	if (Plate != nullptr && PlateRef != nullptr) {
+		//UE_LOG(LogTemp, Warning, TEXT("%s: End overlap with plate: %s"), *GetName(), *Plate->GetName());
+		PlateRef->RemoveCrate(this);
+		PlateRef = nullptr;
+		return;
+	}
+	//UE_LOG(LogTemp, Warning, TEXT("Crate End Overlap: Plate Invalid"));
+
+	// Remove the weight of this crate from the pressure plate
+	// ONLY if this is the crate being removed!
+	ADD_PuzzleCrate* OtherCrate{ Cast<ADD_PuzzleCrate>(OtherActor) };
+	if (OtherCrate != nullptr) {
+		//UE_LOG(LogTemp, Warning, TEXT("%s: End overlap with crate: %s"), *GetName(), *OtherCrate->GetName());
+		int count{ GetOverlappingWeightObjectCount() };
+		if (count <= 1) {
+			if (PlateRef != nullptr) {
+				PlateRef->RemoveCrate(this);
+				PlateRef = nullptr;
+			}
+		}
+		return;
+	}
+	//UE_LOG(LogTemp, Warning, TEXT("Crate End Overlap: Other Crate Invalid"));
+}
+
+int ADD_PuzzleCrate::GetOverlappingWeightObjectCount() {
+	int OLCount{ 0 };
+	TSet<AActor*> OutActors;
+	WeightingCollisionVolume->GetOverlappingActors(OutActors);
+	for (AActor* act : OutActors) {
+		if (Cast<ADD_PuzzleCrate>(act) != nullptr || Cast<ADD_PressurePlate>(act) != nullptr) {
+			OLCount++;
+		}
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Overlapping actor count for %s: %d"), *GetName(), OLCount);
+
+	return OLCount;
+}
